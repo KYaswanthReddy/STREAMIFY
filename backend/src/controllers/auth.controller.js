a85fb2f70bb1b47c985ceb4bb6f1318cb8a42454
Yash <yashwanths19a@gmail.com>
2026-04-18 18:14:42 +0530

    controllers and routers intiliazed

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export async function signup(req, res) {
+    res.send("signup");
+};  
+
+export   async function login   (req, res)  {
+    res.send("login");
+};  
+
+// export const logout = async(req, res) => {
+//     res.send("logout   mmmm");
+// };  
+
+export async function logout(req, res) {
+    res.send("logout   mmmm");
+};  
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PORT=process.env.PORT;
 
+
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}  is running  `);
 });
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
+import authRoutes from "./routes/auth.route.js";
+//app.use("/api/auth",require("./routes/auth.js"));
+app.use("/api/auth",authRoutes);
 
-app.get("/api/auth/signup",(req,res)=>{
-    res.send("signup");
-});
-app.get("/api/auth/logout",(req,res)=>{
-    res.send("logout");
-});
-app.get("/api/auth/login",(req,res)=>{
-    res.send("login");
-});
+// app.get("/",(req,res)=>{
+//     res.send("Hello World");
+// });
+
+// app.get("/api/auth/signup",(req,res)=>{
+//     res.send("signup");
+// });
+// app.get("/api/auth/logout",(req,res)=>{
+//     res.send("logout");
+// });
+// app.get("/api/auth/login",(req,res)=>{
+//     res.send("login");
+// });
