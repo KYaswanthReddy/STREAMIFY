8073bee2d4bcd0dbb492499ce25f55f32521a160
Yash <yashwanths19a@gmail.com>
2026-04-18 17:41:20 +0530

    IN THE BACKEND THE SERVER.JS FILE MOVED TO INSIDE THE SRC FOLDER AND CHANGED THE SCRIPTS SRC/SESRVER.JS AND CREATED RANDOM API LIKE(SIGN UP ,LOGIN, LOGIN OUT).

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+const app=express();
+
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT=process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}  is running  `);
+});
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get("/api/auth/signup",(req,res)=>{
+    res.send("signup");
+});
+app.get("/api/auth/logout",(req,res)=>{
+    res.send("logout");
+});
+app.get("/api/auth/login",(req,res)=>{
+    res.send("login");
+});
